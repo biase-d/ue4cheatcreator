9d459673b2c57c24f0c36858bd63a08291178b20
biase-d <tashingachikwanha@gmail.com>
2024-05-31 15:54:58 +0200

    show when processing cheat file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import Download from '$lib/components/Download.svelte';
+  import Icon from '@iconify/svelte';
 
   /**
    * @type {HTMLInputElement}
@@ -8,9 +9,12 @@
   let cheats = "";
   let size = '';
   let name = ''
+  let loading = false
 
   const handleCFGfile = async () => {
+    loading = true
     const formData = new FormData()
+    // @ts-ignore
     formData.append('cfg', file.files[0])
 
     const response  = await fetch('/api/process', {
@@ -37,14 +41,22 @@
 
 </script>
 
-<h1 class="p-8 font-bold lowercase text-xl"> <a href="/">ue4cheatscreator</a> </h1>
+<h1 class="p-8 font-bold lowercase text-xl flex items-center">
+  <div class="grid items-center justify-center w-[32px] h-[32px]">
+    <Icon icon='pixelarticons:chevrons-horizontal'width=auto/></div>
+    <a href="/">ue4cheatscreator</a>
+</h1>
 
 {#if cheats === ""}
   <form on:submit|preventDefault= {handleCFGfile} class="grid px-8 gap-2.5 items-center justify-center">
     <!-- svelte-ignore a11y-label-has-associated-control -->
     <label class="text-xs">Max Size: 20KB</label>
-    <input type='file' class="file-input file-input-bordered file-input-primary w-full max-w-xs" bind:this={file} accept='.txt' max-size required/>
+    <input type='file' class="file-input file-input-bordered file-input-primary w-full max-w-xs" bind:this={file} accept='.txt' required/>
+    {#if !loading}
     <button class="btn btn-primary max-w-sm" type='submit'> Get Cheats </button>
+    {:else}
+    <button class="btn btn-primary max-w-sm" disabled><span class='animate-spin'><Icon icon="pixelarticons:loader"/> </span></button>
+    {/if}
   </form>
 
 {:else}

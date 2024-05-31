ea970d88f45d1c0a5d0de33b783ec61c07d79946
biase-d <tashingachikwanha@gmail.com>
2024-05-31 15:14:43 +0200

    error handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,17 +28,28 @@
       size = data.size
       name = data.name
       cheats += data.cheats
+    } else if (response.status === 500){
+      alert('File is too big to be a cheat file')
+    } else {
+      alert('Something went wrong. Make sure you are selecting the correct .txt file')
     }
   }
 
 </script>
-<h1> ue4cheatscreator </h1>
+
+<h1 class="p-8 font-bold lowercase text-xl"> ue4cheatscreator </h1>
 
 {#if cheats === ""}
-<form on:submit|preventDefault= {handleCFGfile}>
-  <input type='file' bind:this={file} accept='.txt'/>
-  <button type='submit'> Submit </button>
-</form>
+  <form on:submit|preventDefault= {handleCFGfile} class="grid px-8 gap-2.5 items-center justify-center">
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label class="text-xs">Max Size: 20KB</label>
+    <input type='file' class="file-input file-input-bordered file-input-primary w-full max-w-xs" bind:this={file} accept='.txt' max-size required/>
+    <button class="btn btn-primary max-w-sm" type='submit'> Get Cheats </button>
+  </form>
+
 {:else}
-  <Download cheat={cheats} name={name} />
+<div class="grid items-center justify-center gap-2.5">
+  <Download {cheats} {name} {size}/>
+</div>
+
 {/if}

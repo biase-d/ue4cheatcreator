00836a61b2092d1dffa73bd1752972355f2d9b72
biase-d <tashingachikwanha@gmail.com>
2024-05-31 12:54:57 +0200

    make components

---
+++ src/lib/components/Download.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let cheat; 
+  export let name;
+
+  const downloadTextFile = (cheat) => {
+    const link = document.createElement("a");
+    const text = new Blob([cheat], {type: 'text/plain'});
+    link.href=URL.createObjectURL(text)
+    link.download = name;
+    link.click()
+    URL.
+    revokeObjectURL(link.href)
+  }
+
+</script>
+
+<button on:click={() => downloadTextFile(cheat)}> Download</button>

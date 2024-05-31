945cc51d19cd78fa32373beb19324fffd6fce9c0
biase-d <tashingachikwanha@gmail.com>
2024-05-31 23:39:24 +0200

    add pixel git icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
-  import "../app.css"
+  import Icon from "@iconify/svelte";
+import "../app.css"
 
 </script>
 
 <slot />
 
 <footer class="grid justify-center mt-10">
-  <a href="https://github.com/biase-d/ue4cheatcreator" class="link font-bold lowercase"> Source </a>
+  <a href="https://github.com/biase-d/ue4cheatcreator" class="flex font-bold lowercase"> <Icon icon='pixelarticons:github' width=auto/> Github</a>
 </footer>

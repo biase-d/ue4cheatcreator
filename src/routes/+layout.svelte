f0a5a4f41d6a1e91b842af212df4d63424d335ac
biase-d <tashingachikwanha@gmail.com>
2024-06-15 10:13:46 +0200

    Change github icon color on hover

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,5 +7,5 @@ import "../app.css"
 <slot />
 
 <footer class="grid justify-center mt-10">
-  <a href="https://github.com/biase-d/ue4cheatcreator" class="flex font-bold lowercase"> <Icon icon='pixelarticons:github' width=auto/> Github</a>
+  <a href="https://github.com/biase-d/ue4cheatcreator" class="flex font-bold lowercase hover:text-secondary"> <Icon icon='pixelarticons:github' width=auto/></a>
 </footer>

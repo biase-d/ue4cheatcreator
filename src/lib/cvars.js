38242ec20c824822e99bb758d98f70253900e4e9
biase-d <tashingachikwanha@gmail.com>
2024-06-15 11:37:39 +0200

    Fix typo on foliage cvar

--- src/lib/cvars.js
+++ src/lib/cvars.js
@@ -1,21 +1,45 @@
 import {levels, levelsFlt, levelOptions, toggleOptions, toggleOptionsFlt, fpsTargetOptions, resPercentageOptions, upscaleOptions, AnisotropicFilteringLevels} from '$lib/options'
 
 export let cheatNames = [
-  // --- DYNAMIC RESOLUTION ---
-  { cvar: "r.DynamicRes.OperationMode", name: "Dynamic Resolution", options: toggleOptions},
-  { cvar: "r.DynamicRes.FrameTimeBudget", name: "Dynamic Resolution Target FPS", option: fpsTargetOptions},
-
-  // --- ANTI ALIASING ---
-  { cvar: "r.PostProcessAAQuality", name: "Anti-Aliasing", options: levels },
-  { cvar: "r.TemporalAA.Upsampling", name: "TAAU", options: toggleOptions },
-
-  // --- RENDER RESOLUTION SCALE ---
-  { cvar: "r.ScreenPercentage", name: "Render Resolution", options: resPercentageOptions},
-  { cvar: "r.SecondaryScreenPercentage.GameViewport", name: "2nd Render Resolution", options: resPercentageOptions },
-
-  // --- UPSACLE ---
-  { cvar: "r.Upscale.Quality", name: "Upscale Quality", options: upscaleOptions },
-
+  // DYNAMIC RESOLUTION
+  { 
+    cvar: "r.DynamicRes.OperationMode", 
+    name: "Dynamic Resolution", 
+    options: toggleOptions
+  },
+  { 
+    cvar: "r.DynamicRes.FrameTimeBudget", 
+    name: "Dynamic Resolution Target FPS", 
+    option: fpsTargetOptions
+  },
+  // ANTI ALIASING
+  { 
+    cvar: "r.PostProcessAAQuality", 
+    name: "Anti-Aliasing", 
+    options: levels 
+  },
+  { 
+    cvar: "r.TemporalAA.Upsampling", 
+    name: "TAAU", 
+    options: toggleOptions 
+  },
+  // RENDER RESOLUTION SCALE
+  { 
+    cvar: "r.ScreenPercentage", 
+    name: "Render Resolution", 
+    options: resPercentageOptions
+  },
+  { 
+    cvar: "r.SecondaryScreenPercentage.GameViewport", 
+    name: "2nd Render Resolution", 
+    options: resPercentageOptions 
+  },
+  // UPSCALE
+  { 
+    cvar: "r.Upscale.Quality", 
+    name: "Upscale Quality", 
+    options: upscaleOptions 
+  },
   // --- FIDELITYFX SUPER RESOLUTION ---
   //{ cvar: "r.FidelityFX.FSR.PrimaryUpscale", name: "FSR Primary Upscale"},
   //{ cvar: "r.FidelityFX.FSR.SecondaryUpscale", name: "FSR Secondary Upscale"},
@@ -25,34 +49,71 @@ export let cheatNames = [
   //{ cvar: "r.MobileMSAA", name: "Mobile MSAA", options: ["Temporal AA", "2x MSAA", "4x MSAA", "8x MSAA"] },
   //{ cvar: "r.MSAA.CompositingSampleCount", name: "MSAA Sample Count", options: ["1x", "2x", "4x", "8x"] },
 
-  // --- VIEW DISTANCE ---
-  { cvar: "r.ViewDistanceScale", name: "View Distance Scale", options: levelOptions.slice(0, -2)},
-  { cvar: "foliage.LODDistanceScale", name: "Foliage", option: levelOptions},
-
-
-  // --- EFFECTS ---
-  { cvar: "r.AmbientOcclusionRadiusScale", name: "SSAO", options: levelOptions},
-  { cvar: "r.ShadowQuality", name: "Shadow", options: levels.slice(0, -2)},
-  { cvar: "r.Shadow.DistanceScale", name: "Shadow Distance", options: levelOptions},
-  { cvar: "r.MotionBlurQuality", name: "Motion Blur", options: toggleOptions },
-  { cvar: "r.LensFlareQuality", name: "Lens Flare", options: toggleOptions },
-  { cvar: "r.BloomQuality", name: "Bloom", options: levels },
-  { cvar: "r.SceneColorFringeQuality", name: "Chromatic Aberration", options: toggleOptions },
-  { cvar: "r.DepthOfFieldQuality", name: "Depth of Field", options: toggleOptions},
+  // VIEW DISTANCE
+  { 
+    cvar: "r.ViewDistanceScale", 
+    name: "View Distance Scale", 
+    options: levelOptions.slice(0, -2)
+  },
+  { 
+    cvar: "foliage.LODDistanceScale", 
+    name: "Foliage", 
+    options: levelOptions
+  },
+  // EFFECTS
+  { 
+    cvar: "r.AmbientOcclusionRadiusScale", 
+    name: "SSAO", 
+    options: levelOptions
+  },
+  { 
+    cvar: "r.ShadowQuality", 
+    name: "Shadow", 
+    options: levels.slice(0, -2)
+  },
+  { 
+    cvar: "r.Shadow.DistanceScale", 
+    name: "Shadow Distance", 
+    options: levelOptions
+  },
+  { 
+    cvar: "r.MotionBlurQuality", 
+    name: "Motion Blur", 
+    options: toggleOptions 
+  },
+  { 
+    cvar: "r.LensFlareQuality", 
+    name: "Lens Flare", 
+    options: toggleOptions 
+  },
+  { 
+    cvar: "r.BloomQuality", 
+    name: "Bloom", 
+    options: levels 
+  },
+  { 
+    cvar: "r.SceneColorFringeQuality", 
+    name: "Chromatic Aberration", 
+    options: toggleOptions 
+  },
+  { 
+    cvar: "r.DepthOfFieldQuality", 
+    name: "Depth of Field", 
+    options: toggleOptions
+  },
   //{ cvar: "r.SSR.Quality", name: "SSR Quality", options: ["Off", "Low", "Medium", "High", "Very High"] },
-
   // --- LIGHTING ---
   { cvar: "r.SkyLightingQuality", name: "Sky Lighting Quality", options: toggleOptions},
   { cvar: "r.LightFunctionQuality", name: "Light Function Quality", options: levels.slice(0, -2)},
 
-  // --- POST PROCESSING ---
+  // POST PROCESSING
   { cvar: "r.Tonemapper.GrainQuantization", name: "Film Grain", options: toggleOptions },
   { cvar: "r.Tonemapper.Sharpen", name: "Image Sharpening", options: toggleOptionsFlt},
   { cvar: "r.TonemapperFilm", name: "Filmic Tonemapper", options: toggleOptions},
   { cvar: "r.Tonemapper.Quality", name: "Vignette", options: toggleOptions },
   { cvar: "r.EyeAdaptationQuality", name: "Adaptive Exposure", options: toggleOptions },
 
-  // --- OTHER ---
+  // OTHER
   { cvar: "r.MaxAnisotropy", name: "Anisotropic Filtering", options: AnisotropicFilteringLevels },
   { cvar: "r.Fog", name: "Fog", options: toggleOptions},
   //{ cvar: "r.SSGI.Enable", name: "SSGI Enable"},

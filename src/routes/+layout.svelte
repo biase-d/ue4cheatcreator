ad23995cf94de2455ec5a33d22e6fb3f1c2f827d
biase-d <tashingachikwanha@gmail.com>
2024-09-08 00:24:14 +0200

    remove unused props, move header to +layout.svelte and add some checks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,8 +9,19 @@
   import "../app.css"
 </script>
 
-<slot />
-
-<footer class="grid justify-center mt-10">
-  <a href="https://github.com/biase-d/ue4cheatcreator" class="flex font-bold lowercase hover:text-secondary"> <Icon icon='pixelarticons:github' width=auto/></a>
-</footer>
+<div>
+  <header class="sticky top-0">
+    <nav class="p-8 font-bold lowercase text-xl flex items-center">
+      <div class="grid items-center justify-center w-[32px] h-[32px]">
+        <Icon icon='pixelarticons:chevrons-horizontal'width=auto/>
+      </div>
+      <h1><a href="/">ue4cheatscreator </a></h1>
+    </nav>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer class="grid justify-center mt-10">
+    <a href="https://github.com/biase-d/ue4cheatcreator" class="flex font-bold lowercase hover:text-secondary"> <Icon icon='pixelarticons:github' width=auto/></a>
+  </footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,8 +14,12 @@
   const handleCFGfile = async () => {
     loading = true
     const formData = new FormData()
-    // @ts-ignore
-    formData.append('cfg', file.files[0])
+
+    if (file.files){
+      formData.append('cfg', file.files[0])
+    } else {
+      alert('There is no file to process')
+    }
 
     const response  = await fetch('/api/process', {
       method: 'POST',
@@ -29,11 +33,10 @@
       size = "";
       name = "";
 
-      size = data.size
       name = data.name
       cheats += data.cheats
     } else if (response.status === 500){
-      alert('File is too big to be a cheat file')
+      alert('File is too big. Make sure you are selecting the correct .txt file')
     } else {
       alert('Something went wrong. Make sure you are selecting the correct .txt file')
     }
@@ -43,14 +46,7 @@
     loading = false
     cheats = ""
   }
-
 </script>
-
-<h1 class="p-8 font-bold lowercase text-xl flex items-center">
-  <div class="grid items-center justify-center w-[32px] h-[32px]">
-    <Icon icon='pixelarticons:chevrons-horizontal'width=auto/></div>
-    <a href="/">ue4cheatscreator </a>
-</h1>
 
 {#if cheats === ""}
   <form on:submit|preventDefault= {handleCFGfile} class="grid px-8 gap-2.5 items-center justify-center">
@@ -58,16 +54,16 @@
     <label class="text-xs">Max Size: 20KB</label>
     <input type='file' class="file-input file-input-bordered file-input-primary w-full max-w-xs" bind:this={file} accept='.txt' required/>
     {#if !loading}
-    <button class="btn btn-primary max-w-sm" type='submit'> Get Cheats </button>
+      <button class="btn btn-primary max-w-sm" type='submit'> Get Cheats </button>
     {:else}
-    <button class="btn btn-primary max-w-sm cursor-not-allowed" disabled><span class='animate-spin'><Icon icon="pixelarticons:loader"/> </span></button>
+      <button class="btn btn-primary max-w-sm cursor-not-allowed" disabled><span class='animate-spin'><Icon icon="pixelarticons:loader"/> </span></button>
     {/if}
   </form>
 {:else}
-<div class="grid items-center justify-center gap-2.5">
-  <Download {cheats} {name} {size}/>
-  <button on:click={recreate} class="text-sm font-bold text-primary"> Create for another game </button>
-</div>
+  <div class="grid items-center justify-center gap-2.5">
+    <Download { cheats } { name } />
+    <button on:click={recreate} class="text-sm font-bold text-primary"> Create for another game </button>
+  </div>
 {/if}
 
 <p class="text-center bold text-sm mt-2">Work in progress</p>

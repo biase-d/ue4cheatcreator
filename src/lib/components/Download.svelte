1bd726477a3f15745cde00574ec27c39591a34d6
biase-d <tashingachikwanha@gmail.com>
2024-09-08 00:23:24 +0200

    change variable name

--- src/lib/components/Download.svelte
+++ src/lib/components/Download.svelte
@@ -4,9 +4,9 @@
   export let cheats; 
   export let name;
 
-  const downloadTextFile = (/** @type {BlobPart} */ cheat) => {
+  const downloadCheat = (/** @type {BlobPart} */ cheat) => {
     const link = document.createElement("a");
-    const text = new Blob([cheat], {type: 'text/plain'});
+    const text = new Blob([cheat], { type: 'text/plain' });
     link.href=URL.createObjectURL(text)
     link.download = name;
     link.click()
@@ -15,4 +15,4 @@
   }
 </script>
 
-<button class="btn btn-primary items-center" on:click={() => downloadTextFile(cheats)}><span class="w-[32px] h-[32px] flex items-center"><Icon icon='pixelarticons:cloud-download' width=auto/></span> {name}</button>
+<button class="btn btn-primary items-center" on:click={() => downloadCheat(cheats)}><span class="w-[32px] h-[32px] flex items-center"><Icon icon='pixelarticons:cloud-download' width=auto/></span> {name}</button>

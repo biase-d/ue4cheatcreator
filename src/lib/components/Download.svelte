3286949e3e92fee2310788de633766846cdcd768
biase-d <tashingachikwanha@gmail.com>
2024-09-08 03:33:20 +0200

    add size and new icon

--- src/lib/components/Download.svelte
+++ src/lib/components/Download.svelte
@@ -3,6 +3,7 @@
 
   export let cheats; 
   export let name;
+  export let size
 
   const downloadCheat = (/** @type {BlobPart} */ cheat) => {
     const link = document.createElement("a");
@@ -14,5 +15,10 @@
     revokeObjectURL(link.href)
   }
 </script>
-
-<button class="btn btn-primary items-center" on:click={() => downloadCheat(cheats)}><span class="w-[32px] h-[32px] flex items-center"><Icon icon='pixelarticons:cloud-download' width=auto/></span> {name}</button>
+<!--
+<ul class="">
+  <li class="flex gap-2.5 text-xl"><Icon icon="line-md:file-document" width=auto/> { name }</li>
+  <li class="flex gap-2.5 text-xl"><Icon icon="line-md:hash-small" width=auto/> {size} KB</li>
+</ul>
+-->
+<button class="btn btn-primary items-center" on:click={() => downloadCheat(cheats)}><span class="h-[32] w-[32]"><Icon icon="pixelarticons:download" width="auto"/></span>{name} ({size}KB) </button>

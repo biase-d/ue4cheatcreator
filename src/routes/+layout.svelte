ecf7a83392c3898d5af47f7ca027f535f5adfac0
biase-d <tashingachikwanha@gmail.com>
2024-11-06 08:56:08 +0200

    link to nswitch 60 fps cheats & mods discord, update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,23 +8,23 @@
   import Icon from "@iconify/svelte";
   import "../app.css"
 </script>
-
+<header class="flex justify-center">
+  <nav class="flex gap-2.5 p-5">
+    <h1><a href="/"><Icon icon="icon-park-twotone:game" width=32/></a></h1>
+  </nav>
+</header>
 <div class="grid items-center justify-center gap-2.5">
-  <header class="flex justify-center">
-    <nav class="flex gap-2.5 p-8 font-bold lowercase text-xl">
-      <div class="w-[32px] h-[32px]">
-        <Icon icon="icon-park-twotone:game" width=32/>
-      </div>
-      <h1><a href="/">ue4cheatscreator</a></h1>
-    </nav>
-  </header>
-  <main class='bg-primary/5 p-5 py-10 rounded-md w-96 transition-all border border-primary/15'>
+  <main class='p-5 py-10 rounded-md w-96 transition-all border border-primary/15'>
     <slot />
   </main>
 </div>
 <footer class="flex flex-col mt-10 justify-center items-center w-full gap-5">
-  <span class='text-center label'> Added SSR, TAAU and some fixes </span>
-  <div>
-    <a href="https://github.com/biase-d/ue4cheatcreator" class="flex font-bold lowercase hover:text-secondary transition-all"> <Icon icon='pixelarticons:github' width=auto/></a>
+  <div class='flex flex-col items-center gap-5'>
+    <h2 class='uppercase font-bold'>Links</h2>
+    <div  class='flex flex-col gap-2'>
+      <a href='https://discord.gg/XhwEWVyKer' class='btn' alt='NSwitch 60 FPS Cheats & Mods'><span class='w-[24px] h-[24px]'><Icon icon="line-md:discord-twotone" width='auto'/></span> NSwitch 60 FPS Cheats & Mods</a>
+      <a href="https://github.com/biase-d/ue4cheatcreator" class="btn"><Icon icon="line-md:github-twotone" width='auto'/>Source Code</a>
+    </div>
   </div>
 </footer>
+

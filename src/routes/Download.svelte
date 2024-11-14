22d182cb8bf00068541be4a5e76f735066c308b2
biase-d <tashingachikwanha@gmail.com>
2024-11-14 10:45:11 +0200

    modify component to handle both cheat and patch

--- src/routes/Download.svelte
+++ src/routes/Download.svelte
@@ -1,23 +1,26 @@
 <script>
-  import Icon from '@iconify/svelte';
-
-  export let cheats; 
-  export let name;
-  export let size
+  export let cheat;
+  export let FPSLockerPatch;
+  export let isBeta;
 
   const downloadCheat = (/** @type {BlobPart} */ cheat) => {
     const link = document.createElement("a");
-    const text = new Blob([cheat], { type: 'text/plain' });
+    const text = new Blob([cheat.content], { type: 'text/pain' });
     link.href=URL.createObjectURL(text)
-    link.download = name;
+    link.download = cheat.name;
     link.click()
     URL.
     revokeObjectURL(link.href)
   }
+
 </script>
 <ul class="grid gap-2.5 w-full">
-  <li><span class="label-text">Filename</span> <br>{ name }</li>
-  <li><span class="label-text">Size</span><br>{size} KB</li>
+  <li><span class="label-text">BID</span> <br>{ cheat.name.split('.')[0] }</li>
 </ul>
 
-<button class="btn btn-primary w-full mt-5 font-bold" on:click={() => downloadCheat(cheats)}>Download</button>
+<div>
+  <button class="btn btn-primary w-full mt-5 font-bold" on:click={() => downloadCheat(cheat, 'text/plain')}>Download Cheats ({cheat.size} KB)</button>
+  {#if isBeta}
+    <button class="btn btn-primary w-full mt-5 font-bold" on:click={() => downloadCheat(FPSLockerPatch)}>Download FPSLocker Patch ({FPSLockerPatch.size} KB)</button>
+  {/if}
+</div>

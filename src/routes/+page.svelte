4f8b994eeced8bca95c0f4bd22ddae04102b5de3
biase-d <tashingachikwanha@gmail.com>
2025-01-03 22:31:39 +0200

    attempt to fix undefined files error when not using a custom config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,19 +12,37 @@
   let override = config.config[0]
   let temp;
 
-  async function handleProcessing () {
-    file = {
-      name: await file.files[0].name,
-      content: (await (await file.files[0].text()))
+  async function handleProcessing() {
+    if (file && file.files && file.files.length > 0) {
+      const uploadedFile = file.files[0];
+      const fileName = uploadedFile.name;
+      const fileContent = await uploadedFile.text();
+
+      file = {
+        name: fileName,
+        content: fileContent,
+      };
+    } else {
+      console.error("No file uploaded or 'file.files' is undefined.");
+      return; // Exit early if files don't exist
     }
 
-    if (customConfig.files[0]){
-      temp = yaml.load(await (await customConfig.files[0].text()))
+    if (customConfig && customConfig.files && customConfig.files.length > 0) {
+      try {
+        console.log(customConfig);
+        const configFile = customConfig.files[0];
+        const configText = await configFile.text();
+        temp = yaml.load(configText);
+      } catch (err) {
+        console.error("Error parsing customConfig:", err);
+      }
+    } else {
+      console.warn("No customConfig file uploaded.");
     }
 
-    loaded = true
-    name = (file.name).split('.')[0]
-    content = file.content
+    loaded = true;
+    name = file.name.split('.')[0];
+    content = file.content;
   }
 
   async function generateAndDownload(){
@@ -49,7 +67,7 @@
     <input type='file' class="file-input file-input-bordered file-input-primary w-full max-w-xs" on:change={handleProcessing} bind:this={file} accept='.log' required/>
   </label>
   <div>
-    <button class='link text-primary label-text-alt' on:click={() => {show = true}}>Use custom config</button>
+    <button class='link text-primary label-text-alt' on:click={() => { show = true }}>Use custom config</button>
     {#if show}
       <label class=grid>
         <span>Upload the custom config file to be used for generation</span>
@@ -64,7 +82,7 @@
     <p class='label-text-alt font-bold'>BID</p>
     <p>{name}</p> 
   </div>
-  {#if !show && temp}
+  {#if !(show == true) && !temp}
     <label class='grid label-text-alt gap-2.5'>
       <span class='font-bold'>Group by Categories</span>
       <select bind:value={override.categories} class='select select-bordered'>

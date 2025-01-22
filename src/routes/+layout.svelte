baa24cea84b67db7af99e5e5afbe0663ad4c3026
biase-d <tashingachikwanha@gmail.com>
2025-01-22 18:20:55 +0200

    Disabled categories in the config and updated +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,8 +18,8 @@
     <slot />
   </main>
 </div>
-<footer class="flex flex-col mt-10 justify-center items-center w-full gap-5">
-  <div class='flex flex-col items-center gap-5'>
+<footer class="flex flex-col justify-center items-center w-full">
+  <div class='flex flex-col items-center'>
     <!--
     <h2 class='uppercase font-bold'>Links</h2>
     -->
@@ -27,6 +27,8 @@
       <!--
       <a href='https://discord.gg/XhwEWVyKer' class='btn' alt='NSwitch 60 FPS Cheats & Mods'><span class='w-[24px] h-[24px]'><Icon icon="line-md:discord-twotone" width='auto'/></span> NSwitch 60 FPS Cheats & Mods</a>
       -->
+      <p class='text-sm'>Custom configs, categorisation now supported</p>
+      <p class='text-sm'>NX version will be update soon... kinda busy rn</p>
       <a href="https://github.com/biase-d/ue4cheatcreator" class="btn btn-ghost"><Icon icon="line-md:github-twotone" width='auto'/>Source Code</a>
     </div>
   </div>
